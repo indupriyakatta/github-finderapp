07e4d36a79850de7a7e73be84e077e6be305f906
indu priya <induvasu0305@gmail.com>
2022-07-01 20:31:47 +0530

    initial commit

---
+++ src/components/layout/Spinner.jsx
@@ -0,0 +1,15 @@
+import spinner from './assets/spinner.gif'
+import React from 'react'
+
+function Spinner() {
+  return (
+    <div className='w-100 mt-20' >
+  <img width={180}  
+  className='text-center mx-auto'
+  src={spinner}
+   alt="loading.." />
+    </div>
+  )
+}
+
+export default Spinner
